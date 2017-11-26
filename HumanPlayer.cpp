902c94dbf549973e57494967b032f0137f6aa2bd
Leah Wachtfogel <leah.wach@gmail.com>
2017-11-26 15:59:11 +0200

    interface and human-humen

---
+++ HumanPlayer.cpp
@@ -0,0 +1,4 @@
+//
+// Created by leah on 25/11/17.
+//
+
---
+++ HumanPlayer.h
@@ -0,0 +1,8 @@
+//
+// Created by leah on 25/11/17.
+//
+
+#ifndef REVERSI_HUMANPLAYER_H
+#define REVERSI_HUMANPLAYER_H
+
+#endif //REVERSI_HUMANPLAYER_H
