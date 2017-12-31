b8e4525a2b5d3b168a8b0e8abc4ce8ac71c207d2
Leah Wachtfogel <leah.wach@gmail.com>
2017-12-31 20:04:51 +0200

    no real change

---
+++ ReversiGame/cmake-build-debug/CMakeFiles/ReversiGame.dir/cmake_clean.cmake
@@ -0,0 +1,22 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ReversiGame.dir/src/AI.cpp.o"
+  "CMakeFiles/ReversiGame.dir/src/AiGame.cpp.o"
+  "CMakeFiles/ReversiGame.dir/src/Board.cpp.o"
+  "CMakeFiles/ReversiGame.dir/src/Cell.cpp.o"
+  "CMakeFiles/ReversiGame.dir/src/Client.cpp.o"
+  "CMakeFiles/ReversiGame.dir/src/ClientPlayer.cpp.o"
+  "CMakeFiles/ReversiGame.dir/src/ConsoleDisplay.cpp.o"
+  "CMakeFiles/ReversiGame.dir/src/GameFlow.cpp.o"
+  "CMakeFiles/ReversiGame.dir/src/HumanPlayer.cpp.o"
+  "CMakeFiles/ReversiGame.dir/src/Main.cpp.o"
+  "CMakeFiles/ReversiGame.dir/src/OpponentClientPlayer.cpp.o"
+  "CMakeFiles/ReversiGame.dir/src/RemoteGame.cpp.o"
+  "CMakeFiles/ReversiGame.dir/src/TwoHumansGame.cpp.o"
+  "ReversiGame.pdb"
+  "ReversiGame"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ReversiGame.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
