f31b6710ce46aa6649c51e335f8f31b5937e5586
Leah Wachtfogel <leah.wach@gmail.com>
2018-01-01 10:53:33 +0200

    finished debugging in joingit add *

--- Server/JoinCommand.cpp
+++ Server/JoinCommand.cpp
@@ -5,12 +5,13 @@ pthread_mutex_t mutex_join_game;
 void JoinCommand::execute(string gameName, int socketID, map<string, int> &gameList) {
     pthread_mutex_lock(&mutex_join_game);
 
-    if (gameList.empty()) {//chaecking if list is empty and noticing client
+    if (gameList.empty()) {//checking if list is empty and noticing client
         int empty = -2;
         int w = (int) write(socketID, &empty, sizeof(empty));
         if (w == -1) {
             cout << "Error writing to socket" << endl;
         }
+        pthread_mutex_unlock(&mutex_join_game);
         return;
     }
     if (!gameList.count(gameName)) {//notifying client of invalid game to join
@@ -18,11 +19,13 @@ void JoinCommand::execute(string gameName, int socketID, map<string, int> &gameL
         int w = (int) write(socketID, &fail, sizeof(fail));
         if (w == -1) {
             cout << "Error writing to socket" << endl;
-        		}
+        }
+        pthread_mutex_unlock(&mutex_join_game);
         return;
     }
 
     int firstClientSocket = gameList[gameName];//getting first player's ID
+
     gameList.erase(gameName);// erasing game from list of available games
     pthread_mutex_unlock(&mutex_join_game);
 
